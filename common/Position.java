cf2cb1b6c83599d61c6a785179be15f52631c13a
Starfighter <starfighter@ubuntu.(none)>
2013-10-10 19:44:34 -0400

    added Position.java

---
+++ common/Position.java
@@ -0,0 +1,27 @@
+class Position {
+
+    private int x = 0;
+    private int y = 0;
+
+    void Position(int xPos, int yPos) {
+         x = xPos;
+         y = yPos
+    }
+
+    void getX () {
+         return x;
+    }
+
+    void getX () {
+         return y;
+    }
+
+    void setX (int xPos) {
+         x = xPos;
+    }
+
+    void setY (int yPos) {
+         y = yPos;
+    }
+
+}
