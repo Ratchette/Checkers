00d3506e465529b6dc078a4f30c3742279cb5850
Starfighter <starfighter@ubuntu.(none)>
2013-10-15 19:33:56 -0400

    yup
    new gui and board design

--- src/checkers/Gui.java
+++ src/checkers/Gui.java
@@ -1,73 +1,214 @@
-/**
- * @author Rafael Aquino de Carvalho
- * @author Danielle Fudger
- * @author Ben Douek
- * @author Jennifer Winer
- *
- */
-
-package checkers;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;  //notice javax
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 
 public class Gui implements ActionListener 
 {
-  
-  private JFrame board = new JFrame("Premium Checkers Deluxe 3000");
-  private JButton square[] = new JButton[64];
 
-  public Gui() // the frame constructor method
+  /*Create Window*/
+  private JFrame window = new JFrame("Premium Checkers Deluxe 3000");
+
+  /*Create button array for grids up to 12x12*/
+  private JButton square[] = new JButton[144];
+  private JButton resign = new JButton();
+  private JButton turn = new JButton();
+  private JButton connect = new JButton();
+
+  /*Board info */
+  private int gridSize = 0;
+  private int type = 0;
+  private String gameType;
+  private Image imgP1Piece;
+
+
+
+
+
+  /* Init GUI */
+  public Gui(String game)
   {
+    setBoardType(game);
 
+    /* Setup Window */
+    window.setSize(600,650);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    /* Setup Window, Board and Menu */
+    window.setLayout(new BorderLayout());
+    GridLayout grid = new GridLayout(gridSize,gridSize);
+    GridLayout menu = new GridLayout(1,3);
+    JPanel board = new JPanel(grid);
+    JPanel menuButtons = new JPanel(menu);
+    board.setSize(600,600);
+    menuButtons.setSize(600,50);
+
+    Font font = new Font("Helvetica", Font.PLAIN, 30);
     int checkerLine = 0;
 
-    /*Create Window*/
-    /* Sets size and grid size */
-    board.setSize(600,600);
-    GridLayout grid = new GridLayout(8,8);
-    board.setLayout(grid);
-    board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Font font = new Font("Helvetica", Font.PLAIN, 30);
+    resign.setText("RESIGN");
+    menuButtons.add(resign);
+    connect.setText("CONNECT");
+    menuButtons.add(connect);
+    turn.setText("GO");
+    turn.setBackground(Color.green);
+    menuButtons.add(turn);
 
-
-    /*Add Buttons To The Window*/
-    for(int i=0; i<=63; i++){
+    /*Add Buttons To The Board*/
+    for(int i=0; i<=((gridSize*gridSize)-1); i++){
         
         square[i] = new JButton();
         
-        /* Set Colors of squares */
-        if (i%8 == 0) {
+        /* Set Color of squares depending on type*/
+        if (i%gridSize == 0) {
           checkerLine = 1 + checkerLine;
         }
         if( (i + checkerLine)%2 == 0) {
-          square[i].setBackground(Color.red);
+          if(type == 0) {
+            square[i].setBackground(Color.red);
+          }
+          if(type == 1 || type == 2) {
+            square[i].setBackground(Color.white);
+          }
         } else {
-          square[i].setBackground(Color.black);
+          if(type == 0) {
+            square[i].setBackground(Color.black);
+          }
+          if(type == 1) {
+            square[i].setBackground(Color.red);
+          }
+          if(type == 2){
+            square[i].setBackground(Color.green);
+          }
           square[i].addActionListener(this);
         }
-
         square[i].setFont(font);
         board.add(square[i]);
         square[i].setName( Integer.toString(i) );
     }
 
-    /*Make The Window Visible*/
-    board.setVisible(true);
+    window.getContentPane().add(menuButtons,BorderLayout.NORTH);
+    window.getContentPane().add(board,BorderLayout.CENTER);
+
+    /*Make The Window Visible*/ 
+    window.setVisible(true);
   }
+
 
 
 
   public static void main(String args[]) {
-    new Gui();
+    new Gui("International");
   }
+
+
+
 
   public void actionPerformed(ActionEvent a) {
     JButton pressedButton = (JButton)a.getSource();
-    pressedButton.setBackground(Color.green);
+    try {
+      pressedButton.setIcon(new ImageIcon(img));
+      //pressedButton.setText(pressedButton.getName());
+      turn.setText("STOP");
+      turn.setBackground(Color.red);
+    } catch (Exception ex) {
+    }
   }
 
+
+
+
+
+  private void setBoardType(String game){
+    /* Check Board Type */
+    gameType = game;
+    if(gameType.equalsIgnoreCase("British") || gameType.equalsIgnoreCase("American")) {
+      try {
+        img = scale(ImageIO.read(getClass().getResource("/resources/peice8x8K.png")), 73, 73);
+      } catch (IOException ex) {
+      }
+      gridSize = 8;
+      type = 0;
+    }
+
+    if(gameType.equalsIgnoreCase("Canadian")) {
+      try {
+        img = scale(ImageIO.read(getClass().getResource("/resources/peice8x8K.png")), 50, 50);
+      } catch (IOException ex) {
+      }
+      gridSize = 12;
+      type = 2;
+    }
+
+    if(gameType.equalsIgnoreCase("International")) {
+      try {
+        img = scale(ImageIO.read(getClass().getResource("/resources/peice8x8K.png")), 60, 60);
+      } catch (IOException ex) {
+      }
+      gridSize = 10;
+      type = 1;
+    }
+  }
+
+
+
+  public BufferedImage scale(BufferedImage img, int targetWidth, int targetHeight) {
+
+      int type = (img.getTransparency() == Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+      BufferedImage ret = img;
+      BufferedImage scratchImage = null;
+      Graphics2D g2 = null;
+
+      int w = img.getWidth();
+      int h = img.getHeight();
+
+      int prevW = w;
+      int prevH = h;
+
+      do {
+          if (w > targetWidth) {
+              w /= 2;
+              w = (w < targetWidth) ? targetWidth : w;
+          }
+
+          if (h > targetHeight) {
+              h /= 2;
+              h = (h < targetHeight) ? targetHeight : h;
+          }
+
+          if (scratchImage == null) {
+              scratchImage = new BufferedImage(w, h, type);
+              g2 = scratchImage.createGraphics();
+          }
+
+          g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                  RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+          g2.drawImage(ret, 0, 0, w, h, 0, 0, prevW, prevH, null);
+
+          prevW = w;
+          prevH = h;
+          ret = scratchImage;
+      } while (w != targetWidth || h != targetHeight);
+
+      if (g2 != null) {
+          g2.dispose();
+      }
+
+      if (targetWidth != ret.getWidth() || targetHeight != ret.getHeight()) {
+          scratchImage = new BufferedImage(targetWidth, targetHeight, type);
+          g2 = scratchImage.createGraphics();
+          g2.drawImage(ret, 0, 0, null);
+          g2.dispose();
+          ret = scratchImage;
+      }
+
+      return ret;
+
+  }
 
 }
