4e08bfbeaec9289100878e5eaf9205de729dcca8
rafaelcarv <rafael.aquinodecarvalho@gmail.com>
2013-10-15 21:29:00 -0400

    King moves methods

--- src/checkers/MoveController.java
+++ src/checkers/MoveController.java
@@ -21,6 +21,26 @@ public class MoveController {
 
 		return singleMove;
 	}
+	
+	public SingleMove kingSimpleMove(Piece currentPiece, 
+			Position endPosition) throws Exception {
+
+		Position piecePosition = currentPiece.getPiecePosition();
+		SingleMove singleMove = new SingleMove();
+
+		if (Math.abs(endPosition.getX() - piecePosition.getX()) != 1) {
+			throw new Exception("Invalid move.");
+		}
+
+		if (Math.abs(endPosition.getY() - piecePosition.getY()) != 1) {
+			throw new Exception("Invalid move.");
+		}
+		currentPiece.setPiecePosition(endPosition);
+		singleMove.setEndPosition(endPosition);
+		singleMove.setPieceBeignMoved(currentPiece);
+
+		return singleMove;
+	}
 
 	public SingleMove manFirstJumpMove(Piece currentPiece, 
 			Piece capturedPiece, Position endPosition) throws Exception {
@@ -62,6 +82,56 @@ public class MoveController {
 		}
 
 		if (capturedPiecePosition.getY() - piecePosition.getY() != 1) {
+			return false;
+		}
+
+		if (Math.abs(piecePosition.getX() - capturedPiecePosition.getX()) != 1) {
+			return false;
+		}
+
+		return true;
+	}
+	
+	public SingleMove kingFirstJumpMove(Piece currentPiece, 
+			Piece capturedPiece, Position endPosition) throws Exception {
+
+		Position piecePosition = currentPiece.getPiecePosition();
+		Position capturedPiecePosition = capturedPiece.getPiecePosition();
+		SingleMove singleMove = new SingleMove();
+		
+		if(currentPiece.getColour() == capturedPiece.getColour()) {
+			throw new Exception("Invalid move.");
+		}
+
+		
+		if (kingJumpPositionValidation(endPosition, piecePosition,
+				capturedPiecePosition)) {
+
+			currentPiece.setPiecePosition(endPosition);
+			singleMove.setCapturedPiece(capturedPiece);
+			singleMove.setPieceBeignMoved(currentPiece);
+			singleMove.setEndPosition(endPosition);
+			return singleMove;
+		}
+		
+		else {
+			throw new Exception("Invalid move.");
+		}
+		
+	}
+
+	private Boolean kingJumpPositionValidation(Position endPosition,
+			Position piecePosition, Position capturedPiecePosition) {
+
+		if (Math.abs(endPosition.getX() - piecePosition.getX()) != 2) {
+			return false;
+		}
+
+		if (Math.abs(endPosition.getY() - piecePosition.getY()) != 2) {
+			return false;
+		}
+
+		if (Math.abs(capturedPiecePosition.getY() - piecePosition.getY()) != 1) {
 			return false;
 		}
 
