b6105ad942ea6a3c95670119c72e6200a41ce09c
rafaelcarv <rafael.aquinodecarvalho@gmail.com>
2013-11-08 03:18:24 -0500

    Fixed king's move.

--- src/checkers/Board.java
+++ src/checkers/Board.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
 
 public class Board implements Remote, Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final int NUM_PIECES = 36; // the maximum number of pieces in any game
 
 	private BoardDesign boardDesign;
@@ -29,22 +29,22 @@ public class Board implements Remote, Serializable {
 	 * 
 	 * The top of the board is y=0, so to move pieces down, ADD to y
 	 */
-	
-	
+
+
 	// regular constructor
 	public Board(int gameType) throws Exception {
 		int currentPiece;
 		int i, j;
-		
+
 		boardDesign = new BoardDesign(gameType);
-		
+
 		// initialize the pieces
 		piecePlacement = new Piece[NUM_PIECES];
 		for(i=0; i<piecePlacement.length; i++)
 			piecePlacement[i] = null;
-		
+
 		currentPiece = 0;
-		
+
 		// create all of the black pieces
 		for (i = 0; i < 3; i++) {
 			for (j = 0; j < boardDesign.gridSize; j++) {
@@ -67,12 +67,12 @@ public class Board implements Remote, Serializable {
 			}
 		}
 	}
-	
+
 	// Copy constructor
 	public Board(Board copy) throws RemoteException {
 		boardDesign = new BoardDesign(copy.getBoardDesign());
 		Piece[] copyPieces = copy.getPiecePlacement();
-		
+
 		piecePlacement = new Piece[NUM_PIECES];
 		for (int i = 0; i < piecePlacement.length && copyPieces[i] != null; i++) {
 			piecePlacement[i] = new Piece(copyPieces[i]);
@@ -120,10 +120,10 @@ public class Board implements Remote, Serializable {
 			return false;
 		}
 	}
-	
+
 	public Piece getPieceAtPosition(int x, int y){
 		Position pos;
-		
+
 		for(int i=0; i<piecePlacement.length; i++){
 			if (piecePlacement[i] != null) {
 				pos = piecePlacement[i].getPiecePosition();
@@ -131,13 +131,13 @@ public class Board implements Remote, Serializable {
 					return piecePlacement[i];
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public int getPieceIndex(Position p, Piece[] pArray){
 		Position pos;
-		
+
 		for(int i=0; i<pArray.length; i++){
 			if (pArray[i] != null) {
 				pos = pArray[i].getPiecePosition();
@@ -145,17 +145,17 @@ public class Board implements Remote, Serializable {
 					return i;
 			}
 		}
-		
+
 		return -1;
 	}
-	
-	
+
+
 
 	// FIXME this function must be rewritten
 	public ArrayList<Move> getPossibleMoves(Piece piece) throws Exception {
-		
+
 		ArrayList<Move> moves = new ArrayList<Move>();
-		
+
 		//Basic jump West
 		if( checkWestDiagonal(piece, 1) && !checkWestDiagonal(piece, 2)){
 			int x = getWestDiagonal(piece, 1).getX();
@@ -169,8 +169,8 @@ public class Board implements Remote, Serializable {
 		else if( !checkWestDiagonal(piece, 1) ){
 			moves.add(new SingleMove(piece, null, getWestDiagonal(piece, 1)) );
 		}
-		
-		
+
+
 		//Basic jump East
 		if( checkEastDiagonal(piece, 1) && !checkEastDiagonal(piece, 2)){
 			int x = getEastDiagonal(piece, 1).getX();
@@ -184,123 +184,195 @@ public class Board implements Remote, Serializable {
 		else if( !checkEastDiagonal(piece, 1) ){
 			moves.add(new SingleMove(piece, null, getEastDiagonal(piece, 1)) );
 		}
-		
+
 		/* KING LOGIC */
 		if(piece.isCrown()){
-//			changeColor(piece);
 			//Basic jump West
-			if( checkWestDiagonal(piece, 1) && !checkWestDiagonal(piece, 2)){
-				int x = getWestDiagonal(piece, 1).getX();
-				int y = getWestDiagonal(piece, 1).getY();
+			if( checkKingWestDiagonal(piece, 1) && !checkKingWestDiagonal(piece, 2)){
+				int x = getKingWestDiagonal(piece, 1).getX();
+				int y = getKingWestDiagonal(piece, 1).getY();
 				Piece temp = getPieceAtPosition(x, y);
 				if(temp.getColour() != piece.getColour()){
-					moves.add(new SingleMove(piece, temp, getWestDiagonal(piece, 2) ) );
+					moves.add(new SingleMove(piece, temp, getKingWestDiagonal(piece, 2) ) );
 				}
 			}
 			//Basic West Move logic no jumping
-			else if( !checkWestDiagonal(piece, 1) ){
-				moves.add(new SingleMove(piece, null, getWestDiagonal(piece, 1)) );
+			else if( !checkKingWestDiagonal(piece, 1) ){
+				moves.add(new SingleMove(piece, null, getKingWestDiagonal(piece, 1)) );
 			}
-			
-			
+
+
 			//Basic jump East
-			if( checkEastDiagonal(piece, 1) && !checkEastDiagonal(piece, 2)){
-				int x = getEastDiagonal(piece, 1).getX();
-				int y = getEastDiagonal(piece, 1).getY();
+			if( checkKingEastDiagonal(piece, 1) && !checkKingEastDiagonal(piece, 2)){
+				int x = getKingEastDiagonal(piece, 1).getX();
+				int y = getKingEastDiagonal(piece, 1).getY();
 				Piece temp = getPieceAtPosition(x, y);
 				if( piece.getColour() != temp.getColour() ){
-					moves.add(new SingleMove(piece, temp, getEastDiagonal(piece, 2) ) );
+					moves.add(new SingleMove(piece, temp, getKingEastDiagonal(piece, 2) ) );
 				}
 			}
 			//Basic East Move logic no jumping
-			else if( !checkEastDiagonal(piece, 1) ){
-				moves.add(new SingleMove(piece, null, getEastDiagonal(piece, 1)) );
+			else if( !checkKingEastDiagonal(piece, 1) ){
+				moves.add(new SingleMove(piece, null, getKingEastDiagonal(piece, 1)) );
 			}
-//			changeColor(piece);
 		}
-		
+
+
 		return moves;
 	}
-	
-	
-	
+
+
+
 	//Returns true if position is occupied
 	public boolean checkWestDiagonal(Piece p, int space){
 		Position pos = p.getPiecePosition();
 		int x = pos.getX() - space;
 		int y = -1;
-		
+
 		if(p.getColour() == Piece.BLACK)
 			y = pos.getY() - space;
 		if(p.getColour() == Piece.WHITE)
 			y = pos.getY() + space;
-		
+
 		Position posDiagonal = new Position(x,y);
-		
-		
+
+
 		if(positionOccupied(posDiagonal)  || x >= boardDesign.gridSize || y >= boardDesign.gridSize || x < 0 || y < 0){
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private Position getWestDiagonal(Piece p, int space){
 		Position pos = p.getPiecePosition();
 		int x = pos.getX() - space;
 		int y = -1;
-		
+
 		if(p.getColour() == Piece.BLACK)
 			y = pos.getY() - space;
 		if(p.getColour() == Piece.WHITE)
 			y = pos.getY() + space;
-		
+
 		Position posDiagonal = new Position(x,y);
-		
+
 		return posDiagonal;
 	}
 	
-	
+	//Returns true if position is occupied
+	public boolean checkKingWestDiagonal(Piece p, int space){
+		Position pos = p.getPiecePosition();
+		int x = pos.getX() - space;
+		int y = -1;
+
+		if(p.getColour() == Piece.BLACK)
+			y = pos.getY() + space;
+		if(p.getColour() == Piece.WHITE)
+			y = pos.getY() - space;
+
+		Position posDiagonal = new Position(x,y);
+
+
+		if(positionOccupied(posDiagonal)  || x >= boardDesign.gridSize || y >= boardDesign.gridSize || x < 0 || y < 0){
+			return true;
+		}
+
+		return false;
+	}
+
+	private Position getKingWestDiagonal(Piece p, int space){
+		Position pos = p.getPiecePosition();
+		int x = pos.getX() - space;
+		int y = -1;
+
+		if(p.getColour() == Piece.BLACK)
+			y = pos.getY() + space;
+		if(p.getColour() == Piece.WHITE)
+			y = pos.getY() - space;
+
+		Position posDiagonal = new Position(x,y);
+
+		return posDiagonal;
+	}
+
+
+
 	public boolean checkEastDiagonal(Piece p, int space){
 		Position pos = p.getPiecePosition();
 		int x = pos.getX() + space;
 		int y = -1;
-		
+
 		if(p.getColour() == Piece.BLACK)
 			y = pos.getY() - space;
 		if(p.getColour() == Piece.WHITE)
 			y = pos.getY() + space;
-		
+
 		Position posDiagonal = new Position(x,y);
-		
-		
+
+
 		if(positionOccupied(posDiagonal) || x >= boardDesign.gridSize || y >= boardDesign.gridSize || x < 0 || y < 0){
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private Position getEastDiagonal(Piece p, int space){
 		Position pos = p.getPiecePosition();
 		int x = pos.getX() + space;
 		int y = -1;
-		
+
 		if(p.getColour() == Piece.BLACK)
 			y = pos.getY() - space;
 		if(p.getColour() == Piece.WHITE)
 			y = pos.getY() + space;
-		
+
 		Position posDiagonal = new Position(x,y);
-		
+
 		return posDiagonal;
 	}
-	
-	
+
+
+	public boolean checkKingEastDiagonal(Piece p, int space){
+		Position pos = p.getPiecePosition();
+		int x = pos.getX() + space;
+		int y = -1;
+
+		if(p.getColour() == Piece.BLACK)
+			y = pos.getY() + space;
+		if(p.getColour() == Piece.WHITE)
+			y = pos.getY() - space;
+
+		Position posDiagonal = new Position(x,y);
+
+
+		if(positionOccupied(posDiagonal) || x >= boardDesign.gridSize || y >= boardDesign.gridSize || x < 0 || y < 0){
+			return true;
+		}
+
+		return false;
+	}
+
+	private Position getKingEastDiagonal(Piece p, int space){
+		Position pos = p.getPiecePosition();
+		int x = pos.getX() + space;
+		int y = -1;
+
+		if(p.getColour() == Piece.BLACK)
+			y = pos.getY() + space;
+		if(p.getColour() == Piece.WHITE)
+			y = pos.getY() - space;
+
+		Position posDiagonal = new Position(x,y);
+
+		return posDiagonal;
+	}
+
+
 	public boolean validateMove(Move move) throws RemoteException{
 		Piece p = move.moveSequence()[0].getPieceBeignMoved();
 		int finalMoveP = move.moveSequence().length - 1;
-		
+
 		try {
 			ArrayList<Move> moves = getPossibleMoves(p);
 			for (int i = 0; i < moves.size(); i++) {
@@ -317,13 +389,13 @@ public class Board implements Remote, Serializable {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
+
 		return false;
 	}
-	
-	
-	
+
+
+
 	private boolean positionOccupied(Position pos){
 		for (int i = 0; i < piecePlacement.length; i++){
 			if(piecePlacement[i] != null){
@@ -346,29 +418,29 @@ public class Board implements Remote, Serializable {
 		Scanner file;
 		String inputLine;
 		String[] squares;
-		
+
 		int boardSize;
 		int currentPiece;
 		int i, j;
-		
+
 		file = new Scanner(new BufferedReader(new FileReader(filename)));
-		
+
 		// Assumes that the first line in the file is the Board design
 		inputLine = file.nextLine().trim();
 		boardDesign = new BoardDesign(inputLine);
 		boardSize = boardDesign.gridSize;	
-		
+
 		// initialize the pieces
 		piecePlacement = new Piece[NUM_PIECES];
 		for(i=0; i<piecePlacement.length; i++)
 			piecePlacement[i] = null;
 		currentPiece = 0;
-		
+
 		// import pieces
 		for(i=0; i<boardSize; i++){
 			inputLine = file.nextLine().trim();
 			squares = inputLine.split(",");
-			
+
 			for(j=0; j<boardSize; j++){
 				if(squares[j].equals("@"))
 					piecePlacement[currentPiece++] = new Piece(new Position(j, i), false, Piece.BLACK);
--- src/functionalTests/EachChoiceTest.java
+++ src/functionalTests/EachChoiceTest.java
@@ -13,6 +13,11 @@ public class EachChoiceTest {
 
 	private Board board;
 	
+	/**
+	 * Test if a pawn in the center of the boardcould move to a invalid place
+	 * in the board, with no piece on it.
+	 * Expects that the validateMove method return false, because it is a invalid move.
+	 */
 	@Test
 	public void testPawnNoPieceInTheWayInvalidMove() {
 		try {
@@ -28,10 +33,15 @@ public class EachChoiceTest {
 		}
 	}
 	
+	/**
+	 * Test if a pawn in the left edge could move to the right diagonal forward place, 
+	 * which already has another piece.
+	 * Expects that the validateMove method return false, because it is a invalid move.
+	 */
 	@Test
 	public void testPawnLeftEdgeMoveRightSpotTake() {
 		try {
-			board = new Board("testCaseBoards/centerBoard.csv");
+			board = new Board("testCaseBoards/leftEdgeBoard.csv");
 			Piece piece = board.getPiecePlacement()[0];
 			piece.setCrown(false);
 			Position initialPosition = piece.getPiecePosition();
@@ -45,4 +55,110 @@ public class EachChoiceTest {
 		}
 	}
 	
+	/**
+	 * Test if a pawn on the right edge of the board could move to the diagonal left forward which 
+	 * has a piece with the same colour.
+	 * Expects that the validateMove method return false, because it is a invalid move.
+	 */
+	@Test
+	public void testPawnRightEdgeMoveLeftSameColour() {
+		try {
+			board = new Board("testCaseBoards/rightEdgeBoard.csv");
+			Piece piece = board.getPiecePlacement()[0];
+			piece.setCrown(false);
+			Position initialPosition = piece.getPiecePosition();
+			Position endPosition = new Position(initialPosition.getX() - 1, initialPosition.getY() - 1);
+			Piece otherPiece = new Piece(endPosition, false, Piece.WHITE);
+			board.getPiecePlacement()[1] = otherPiece;
+			SingleMove move = new SingleMove(piece, null, endPosition);
+			TestCase.assertFalse(board.validateMove(move));
+		} catch (Exception e) {
+			TestCase.fail();
+		}
+	}
+	
+	/**
+	 * Test if a king piece on the top edge of the board could jump 
+	 * an opponent piece in a diagonal left backward.
+	 * Expects that the test that  the method validateMove return true, because for a king it is a valid move.
+	 * This test found a bug, which the King was not moving or jumping backward properly.
+	 */
+	@Test
+	public void testKingTopEdgeJumpOpponentDownLeft() {
+		try {
+			board = new Board("testCaseBoards/topEdgeBoard.csv");
+			Piece piece = board.getPiecePlacement()[0];
+			piece.setCrown(true);
+			Position initialPosition = piece.getPiecePosition();
+			Position endPosition = new Position(initialPosition.getX() - 2, initialPosition.getY() + 2);
+			Piece otherPiece = new Piece(new Position(initialPosition.getX() - 1, initialPosition.getY() + 1), false, Piece.WHITE);
+			board.getPiecePlacement()[1] = otherPiece;
+			SingleMove move = new SingleMove(piece, otherPiece, endPosition);
+			TestCase.assertTrue(board.validateMove(move));
+		} catch (Exception e) {
+			TestCase.fail();
+		}
+	}
+	
+	/**
+	 * Test if the king on the bottom edge of the board could move left backward.
+	 * This test expects that the return of validateMove method return false, because it
+	 * is trying to move off board, and it is an invalid move.
+	 */
+	@Test
+	public void testKingBottomEdgeMoveDownLeft() {
+		try {
+			board = new Board("testCaseBoards/bottomEdgeBoard.csv");
+			Piece piece = board.getPiecePlacement()[0];
+			piece.setCrown(true);
+			Position initialPosition = piece.getPiecePosition();
+			Position endPosition = new Position(initialPosition.getX() - 1, initialPosition.getY() + 1);
+			SingleMove move = new SingleMove(piece, null, endPosition);
+			TestCase.assertFalse(board.validateMove(move));
+		} catch (Exception e) {
+			TestCase.fail();
+		}
+	}
+	
+	/**
+	 * Test if the king could jump to an spot already taken.
+	 * The return of the method validate move is expected to return false.
+	 */
+	@Test
+	public void testKingAlmostRightEdgeJumpLeftSpotTaken() {
+		try {
+			board = new Board("testCaseBoard/almostRightEdgeBoard.csv");
+			Piece piece = board.getPiecePlacement()[0];
+			piece.setCrown(false);
+			Position initialPosition = piece.getPiecePosition();
+			Position endPosition = new Position(initialPosition.getX() - 2, initialPosition.getY() - 2);
+			Piece otherPiece = new Piece(new Position(initialPosition.getX() - 2, initialPosition.getY() - 2), false, Piece.WHITE);
+			board.getPiecePlacement()[1] = otherPiece;
+			SingleMove move = new SingleMove(piece, otherPiece, endPosition);
+			TestCase.assertFalse(board.validateMove(move));
+		} catch (Exception e) {
+			TestCase.fail();
+		}
+	}
+	
+	/**
+	 * Test if the king could move to a place that is already occupied by a piece with the same colour.
+	 * The return of the method validateMove is expected to be false.
+	 */
+	@Test
+	public void testKingAlmostLeftEdgeMoveDownRightSameColour() {
+		try {
+			board = new Board("testCaseBoards/almostLeftEdgeBoard.csv");
+			Piece piece = board.getPiecePlacement()[0];
+			piece.setCrown(true);
+			Position initialPosition = piece.getPiecePosition();
+			Position endPosition = new Position(initialPosition.getX() + 1, initialPosition.getY() - 1);
+			Piece otherPiece = new Piece(endPosition, false, Piece.WHITE);
+			board.getPiecePlacement()[1] = otherPiece;
+			SingleMove move = new SingleMove(piece, null, endPosition);
+			TestCase.assertFalse(board.validateMove(move));
+		} catch (Exception e) {
+			TestCase.fail();
+		}
+	}
 }
